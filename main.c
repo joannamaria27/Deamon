719b3547e63610abe09444aad1b7a93e9651ac48
joannamaria27 <49189136+joannamaria27@users.noreply.github.com>
2019-04-09 21:11:14 +0200

    Create min.c
    Stworzenie main.c, 
    sprawdzanie czy plik - nie działa prawidłowo
    sprawdzanie czy katalog - nie działa prawidłowo
    deamon czekający okreslony statycznie czas + zapis uruchomienia demona do logu

---
+++ main.c
@@ -0,0 +1,160 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/syslog.h>
+#include <sys/mman.h>
+#include <stdbool.h>
+
+bool is_dir(const char* path) {
+    struct stat buf;
+    stat(path, &buf);
+    return S_ISDIR(buf.st_mode);
+}
+bool is_file(const char* path) {
+ struct stat buf;
+ stat(path, &buf);
+ return S_ISREG(buf.st_mode);
+}
+
+volatile int sygnal = 1;
+
+int czas = 5; //*60 = 5minut - zmienić!!!
+
+int main(int argc, char **argv)  
+{
+
+if(argc<1 ) // dod arg by zmienic
+    {
+    	printf ("błąd: %s ", strerror (errno)); 
+	    return 1;
+
+    }
+
+
+
+printf("%d\n",czas);
+
+printf("Dziwne rzeczy\n");
+
+
+//chyba nie działa/
+//if(is_dir(argv[1]) == true) //sciezka zr nie jest katalogiem
+if(is_file(argv[1]) == true)
+{
+
+
+    printf("Scieżka zródłowa nie jest katalogiem\n");
+    return -1;
+
+}
+//if(is_dir(argv[2]) == true)  //sciezka docel nie jest katalogiem
+if(is_file(argv[2]) == true)
+{
+
+
+    printf("Scieżka docelowa nie jest katalogiem\n");
+    return -1;
+
+}
+printf("Oba żródła są katalogami\n");
+
+
+setlogmask (LOG_UPTO (LOG_NOTICE)); //maksymalny, najważniejszy log jaki można wysłać;
+openlog ("demon-projekt", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);    //pierwszy arg nazwa programiu, pózniej co ma się wypisać oprócz samej wiadomosci chyba?
+
+void handler(int signum)
+{
+    //printf("signal!\n");
+    sygnal=0;
+    
+}
+signal(SIGQUIT, handler);
+int czyObudzonyPrzezPrzycisk=0;
+//if(SIGUSR1 || sleep(czas))
+if(sygnal==0)
+{
+    czas = 0;
+   
+    printf("Dem0n budzi się\n");
+    syslog (LOG_NOTICE, "Demon został obudzony poprzez przycisk\n");
+   // closelog ();   //zamkniecie logu
+}
+else
+{
+    //działa zawsze :c :c :c
+    syslog (LOG_NOTICE, "Demon został obudzony po 5 minutach\n");
+  //  closelog (); 
+}
+
+sleep(czas);
+
+
+
+// while (1) 
+// { 
+ //tu działa :)
+    pid_t p;
+    p=fork();
+    
+    if (p < 0) //obsługa błędóœ pid
+    {
+        exit(EXIT_FAILURE);
+    }
+    if(p==0)
+    {
+
+    
+        execvp("firefox", argv);
+        printf("Dziaaaaaała\n");
+        //sleep(10);
+
+
+
+//do tego miejsca działa
+/*
+    char plikZr, plikDoc;
+    //najpierw przejscie przez pliki i jesli:
+
+    if(is_file(plikZr)==true)
+    {
+        if(plikZr==plikDoc)) //nazwa taka sama???????
+        {
+//hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+        }
+        else if (  )//data sie nie zgadza
+        {
+
+        }
+        else
+        {
+            //brak pliku w fiolderze docelowym
+            //kopiowanie pliku do katalogu docelowego (open,read)
+        }
+
+
+    }
+
+*/
+
+
+    }
+         
+    sleep(5);
+// }
+
+
+closelog ();   //zamkniecie logu
+
+exit(EXIT_SUCCESS);
+
+
+}
